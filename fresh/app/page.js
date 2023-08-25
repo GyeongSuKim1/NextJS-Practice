d846b7d21dd894ac4cbdd5760b6789a1e378b57d
Gyeongsu Kim <rla04722@naver.com>
2023-08-25 18:51:36 +0900

    Feat (main) : 화면 출력 TEST

--- fresh/app/page.js
+++ fresh/app/page.js
@@ -1,7 +1,10 @@
 export default function Home() {
+  let name = 'kim'
+
   return (
     <div>
-      
+      <h4 className="title">Fresh</h4>
+      <p className="title-sub">by. { name }</p>
     </div>
   )
 }
