dec0bef92d17620dd5bed342babfdac90ec5df44
Gyeongsu Kim <rla04722@naver.com>
2023-08-29 17:46:43 +0900

    Feat (test) : /test/check 링크 연결

--- fresh/app/test/page.js
+++ fresh/app/test/page.js
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export default function Test() {
 
     return (
         <div>
+        <Link href="/test/check" style={{color: 'white', textDecoration: 'none', }}>
             <h1>TEST PAGE</h1>
+        </Link>
         </div>
     )
 }
