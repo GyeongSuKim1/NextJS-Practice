dac04f217225b263996567eb87d766701b0d0923
Gyeongsu Kim <rla04722@naver.com>
2023-08-29 18:20:16 +0900

    Feat (favicon) : Favicon설정

--- fresh/app/layout.js
+++ fresh/app/layout.js
@@ -4,15 +4,22 @@ import Link from "next/link"
 export const metadata = {
   title: 'Fresh',
   description: 'My app',
-}
+  icons: {
+    icon: '/favicon.png',
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+
+      <link rel='icon' href='/favicon.png' />
+
       <body>
         <Navber />
         {children}
-        </body>
+      </body>
+
     </html>
   )
 }
@@ -20,10 +27,11 @@ export default function RootLayout({ children }) {
 function Navber() {
   return (
     <div className="navbar">
-    <Link href="/">HOME</Link>
-    <Link href="/list">LIST</Link>
-    <Link href="/cart">CART</Link>
-    <Link href="/test">TEST</Link>
-  </div>
+      <img src='/favicon.png' style={{width: '20px', height: '15px', paddingRight: '20px'}}/>
+      <Link href="/">HOME</Link>
+      <Link href="/list">LIST</Link>
+      <Link href="/cart">CART</Link>
+      <Link href="/test">TEST</Link>
+    </div>
   )
 }
